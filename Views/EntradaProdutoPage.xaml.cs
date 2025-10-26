9735c8820f3d4b8083d6f946423aaee47c9743eb
Evellyn <evellynfrancyne7@hotmail.com>
2025-10-26 14:11:07 -0300

    Adição do Picker nas páginas de entrada e saida de produtos. Funcionalidade nos botões voltar ao inicio, registrar produto e retirar produto.

--- Views/EntradaProdutoPage.xaml.cs
+++ Views/EntradaProdutoPage.xaml.cs
@@ -6,9 +6,28 @@ public partial class EntradaProdutoPage : ContentPage
 	{
 		InitializeComponent();
 	}
-
+    protected override async void OnAppearing()  //Mantem a página sempre atualizada de produtos cadastrados
+    {
+        base.OnAppearing();
+        await CarregarProdutos();
+    }
+    private async Task CarregarProdutos()
+    {
+        var produtos = await App.Database.GetProdutosAsync();
+        pickerprodutos.ItemsSource = produtos;
+    }
     private void EscolhaProduto_SelectedIndexChanged(object sender, EventArgs e)
     {
 
+    }
+
+    private void RegistrarProduto_Clicked(object sender, EventArgs e)  //Botão para Registrar o produto na página de entrada
+    {
+
+    }
+
+    private async void PaginaInicial_Clicked(object sender, EventArgs e) //Botão para voltar à página inicial
+    {
+        await Navigation.PopAsync();
     }
 }
--- Views/SaidaProdutoPage1.xaml.cs
+++ Views/SaidaProdutoPage1.xaml.cs
@@ -2,8 +2,32 @@ namespace MauiApp1.Views;
 
 public partial class SaidaProdutoPage1 : ContentPage
 {
-	public SaidaProdutoPage1()
-	{
-		InitializeComponent();
-	}
+    public SaidaProdutoPage1()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()  //Mantem a página sempre atualizada de produtos cadastrados
+    {
+        base.OnAppearing();
+        await CarregarProdutos();
+    }
+    private async Task CarregarProdutos()
+    {
+        var produtos = await App.Database.GetProdutosAsync();
+        pickerprodutos.ItemsSource = produtos;
+    }
+    private void EscolhaProduto_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void SaidaProduto_Clicked(object sender, EventArgs e) //Botão para Retirar,  o produto
+    {
+      
+    }
+
+    private async void PaginaInicial_Clicked(object sender, EventArgs e) //Botão para voltar à página inicial
+    {
+        await Navigation.PopAsync();
+    }
 }
